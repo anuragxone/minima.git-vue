5b7494e78a29e16ea441eb1700e93b4e5e17af8f
Anurag Kumar <anuragxone@outlook.in>
2025-07-04 10:53:32 +0000

    feat: add Geist/Mono fonts

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@ import { Button } from '@/components/ui/button'
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div class="wrapper font-sans">
       <HelloWorld msg="You did it!" />
 
       <nav>
